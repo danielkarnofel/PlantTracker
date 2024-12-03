14d51f707aa46242232b639335738365c8d89d91
Dan <karnofeldaniel@protonmail.com>
2024-12-02 16:06:47 -0800

    created Admin activity

---
+++ app/src/main/java/com/example/planttracker/AdminActivity.java
@@ -0,0 +1,19 @@
+package com.example.planttracker;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.planttracker.databinding.ActivityAdminBinding;
+
+public class AdminActivity extends AppCompatActivity {
+
+    private ActivityAdminBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
