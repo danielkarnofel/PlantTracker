30875d96e8cbfa7c475ade5426e31a35e45a0923
josea <joserodriguez0399@gmail.com>
2024-12-04 19:25:39 -0800

    Added generated code, setters, getters, toString() and hashCode to User entity. Created the UserDAO, and began to implement UserDao in AppRepository.java and AppDatabase.java. The attempted completion of User and UserDao follows HW4 a lot. Also changed the AppDatabase.java class signature to extend the RoomDatabase class. My attempt to implement User and UserDao created a problem with an ignored file AppDatabase_Impl.java. Rebased to resolve merge conflicts.

--- app/src/main/java/com/example/planttracker/database/AppRepository.java
+++ app/src/main/java/com/example/planttracker/database/AppRepository.java
@@ -2,6 +2,10 @@ package com.example.planttracker.database;
 
 import android.app.Application;
 
+import androidx.lifecycle.LiveData;
+
+import com.example.planttracker.database.entities.User;
+
 public class AppRepository {
     private final AreaDAO areaDAO;
     private final PlantDAO plantDAO;
@@ -16,6 +20,21 @@ public class AppRepository {
     }
 
     // TODO: implement getRepository method using singleton patter and a Future
+
+    /// *** Code borrowed from HW4 ***
+    public void insertUser(User... user){
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            userDAO.insert(user);
+        });
+    }
+
+    public LiveData<User> getUserByUserName(String username) {
+        return userDAO.getUserByUserName(username);
+    }
+    public LiveData<User> getUserByUserId(int userId) {
+        return userDAO.getUserByUserId(userId);
+    }
+    /// *** End code borrowed from H4 ***
     public static AppRepository getRepository(Application application) {
         return null;
     }
