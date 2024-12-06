231a65b1c3b8d55dad1918d5ca4c23bf6879d919
Dan <karnofeldaniel@protonmail.com>
2024-12-06 13:57:16 -0800

    implemented the getRepository method in the AppRepository class, added insert and delete methods for all entities

--- app/src/main/java/com/example/planttracker/database/AppRepository.java
+++ app/src/main/java/com/example/planttracker/database/AppRepository.java
@@ -1,10 +1,17 @@
 package com.example.planttracker.database;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 
+import com.example.planttracker.database.entities.Area;
+import com.example.planttracker.database.entities.Plant;
 import com.example.planttracker.database.entities.User;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 public class AppRepository {
     private final AreaDAO areaDAO;
@@ -19,23 +26,77 @@ public class AppRepository {
         this.userDAO = db.userDAO();
     }
 
-    // TODO: implement getRepository method using singleton patter and a Future
+    public static AppRepository getRepository(Application application) {
+        if (repository != null) {
+            return repository;
+        }
+        Future<AppRepository> future = AppDatabase.databaseWriteExecutor.submit(
+                new Callable<AppRepository>() {
+                    @Override
+                    public AppRepository call() throws Exception {
+                        return new AppRepository(application);
+                    }
+                }
+        );
+        try {
+            return future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO: add a log here
+        }
+        return null;
+    }
 
-    /// *** Code borrowed from HW4 ***
-    public void insertUser(User... user){
+    // Area methods
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    public void insertArea(Area... area) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            areaDAO.insert(area);
+        });
+    }
+
+    public void deleteArea(Area area) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            areaDAO.delete(area);
+        });
+    }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    // Plant methods
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    public void insertPlant(Plant... plant) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            plantDAO.insert(plant);
+        });
+    }
+
+    public void deletePlant(Plant plant) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            plantDAO.delete(plant);
+        });
+    }
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    // User methods
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    public void insertUser(User... user) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
             userDAO.insert(user);
+        });
+    }
+
+    public void deleteUser(User user) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            userDAO.delete(user);
         });
     }
 
     public LiveData<User> getUserByUserName(String username) {
         return userDAO.getUserByUserName(username);
     }
+
     public LiveData<User> getUserByUserId(int userId) {
         return userDAO.getUserByUserId(userId);
     }
-    /// *** End code borrowed from H4 ***
-    public static AppRepository getRepository(Application application) {
-        return null;
-    }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
--- app/src/main/java/com/example/planttracker/database/UserDAO.java
+++ app/src/main/java/com/example/planttracker/database/UserDAO.java
@@ -13,22 +13,24 @@ import java.util.List;
 
 @Dao
 public interface UserDAO {
-    /// *** Some convenience  methods provided by Room used in HW 4 ***
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User... user); // Will take zero or more User user arguments.
 
-    /// ***  Query methods borrowed from HW4, can be helpful for our project ***
     @Delete
     void delete(User user);
+
     // Get all users, ordered by username.
     @Query("SELECT * FROM " + AppDatabase.USER_TABLE + " ORDER BY username")
     LiveData<List<User>> getAllUsers();
+
     // Will delete all users.
     @Query("DELETE from " + AppDatabase.USER_TABLE)
     void deleteAll();
+
     // Will select a user by username.
     @Query("SELECT * from " + AppDatabase.USER_TABLE + " WHERE username == :username")
     LiveData<User> getUserByUserName(String username);
+
     // Will select a user by userID.
     @Query("SELECT * from " + AppDatabase.USER_TABLE + " WHERE userID == :userId")
     LiveData<User> getUserByUserId(int userId);
