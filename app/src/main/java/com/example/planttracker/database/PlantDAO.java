0aa656f40f08602dec200601803a84e2d64a7680
Dan <karnofeldaniel@protonmail.com>
2024-12-06 13:34:22 -0800

    added some basic methods to the PlantDAO class

--- app/src/main/java/com/example/planttracker/database/PlantDAO.java
+++ app/src/main/java/com/example/planttracker/database/PlantDAO.java
@@ -1,8 +1,24 @@
 package com.example.planttracker.database;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.planttracker.database.entities.Plant;
 
 @Dao
 public interface PlantDAO {
 
+    @Insert
+    void insert(Plant... plant);
+
+    @Delete
+    void delete(Plant plant);
+
+    @Query("DELETE FROM " + AppDatabase.PLANT_TABLE)
+    void deleteAll();
+
+    @Query("SELECT * FROM " + AppDatabase.PLANT_TABLE)
+    void getAllPlants();
 }
