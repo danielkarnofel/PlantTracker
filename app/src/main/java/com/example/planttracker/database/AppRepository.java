826bb58c41f9983bc3b462a3a8c1f29dc416e1cc
Dan <karnofeldaniel@protonmail.com>
2024-12-05 16:17:41 -0800

    deleted the AppDAO class created individual DAO classes for User, Plant, and Area entities: UserDAO.java, PlantDAO.java, AreaDAO.java replaced the abstract appDAO() method in AppDatabase.java with 3 new abstract methods: userDAO(), plantDAO(), areaDAO() replaced the private final field appDAO in AppRepository.java with 3 new private final fields: userDAO, plantDAO, areaDAO updated the constructor in AppRepository.java to instantiate the new fields

--- app/src/main/java/com/example/planttracker/database/AppRepository.java
+++ app/src/main/java/com/example/planttracker/database/AppRepository.java
@@ -3,12 +3,16 @@ package com.example.planttracker.database;
 import android.app.Application;
 
 public class AppRepository {
-    private final AppDAO appDAO;
+    private final AreaDAO areaDAO;
+    private final PlantDAO plantDAO;
+    private final UserDAO userDAO;
     private static AppRepository repository;
 
     private AppRepository(Application application) {
         AppDatabase db = AppDatabase.getDatabase(application);
-        this.appDAO = db.appDAO();
+        this.areaDAO = db.areaDAO();
+        this.plantDAO = db.plantDAO();
+        this.userDAO = db.userDAO();
     }
 
     // TODO: implement getRepository method using singleton patter and a Future
