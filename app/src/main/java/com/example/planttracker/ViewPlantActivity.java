1e854d496ed183b1a73c2412bd4db1c2b800ac3a
jhansen <jon.hansen@csumb.edu>
2024-12-12 08:17:22 -0800

    Added functionality to viewPlantActivity Water Plant button. Button will now call the setter for the currently-selected Plant's lastWatered, and set it to the current system date.

--- app/src/main/java/com/example/planttracker/ViewPlantActivity.java
+++ app/src/main/java/com/example/planttracker/ViewPlantActivity.java
@@ -13,8 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 
 import com.example.planttracker.database.AppRepository;
+import com.example.planttracker.database.entities.Plant;
 import com.example.planttracker.database.entities.User;
 import com.example.planttracker.databinding.ActivityViewPlantBinding;
+
+import java.time.LocalDateTime;
 
 public class ViewPlantActivity extends AppCompatActivity {
     private ActivityViewPlantBinding binding;
@@ -22,6 +25,7 @@ public class ViewPlantActivity extends AppCompatActivity {
     private User loggedInUser;
     private AppRepository repository;
     private int selectedPlantID;
+    public Plant selectedPlant;
     static final String VIEW_PLANT_ACTIVITY_SELECTED_PLANT_ID_EXTRA_KEY = "com.example.planttracker.VIEW_PLANT_ACTIVITY_SELECTED_PLANT_ID_EXTRA_KEY";
 
     @Override
@@ -45,9 +49,22 @@ public class ViewPlantActivity extends AppCompatActivity {
             }
         });
 
-        // TODO: implement onClick for Water Plant button
+        LiveData<Plant> plantObserver = repository.getPlantByID(selectedPlantID);
+        plantObserver.observe(this, plant -> {
+            this.selectedPlant = plant;
+            if (plant != null) {
+                invalidateOptionsMenu();
+            }
+        });
 
-        selectedPlantID = 1;
+        // TODO: implement onClick for Water Plant button
+        binding.viewPlantActivityWaterPlantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedPlant.setLastWatered(LocalDateTime.now());
+            }
+        });
+
         // implement onClick for Edit button
         binding.viewPlantActivityEditButton.setOnClickListener(new View.OnClickListener() {
             @Override
