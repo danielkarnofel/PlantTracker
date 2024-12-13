54c3acb22a79bb7f22946c596c0c558f5bc679ba
eshandilcsumb <eshandil@csumb.edu>
2024-12-12 23:49:10 -0800

    Implemented area test. All tests passed. Cleaned user test and removed unused lines of code.

--- app/src/androidTest/java/com/example/planttracker/DatabaseUserTest.java
+++ app/src/androidTest/java/com/example/planttracker/DatabaseUserTest.java
@@ -14,13 +14,8 @@ import androidx.room.Room;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.planttracker.database.AppDatabase;
-import com.example.planttracker.database.AreaDAO;
-import com.example.planttracker.database.PlantDAO;
 import com.example.planttracker.database.UserDAO;
-import com.example.planttracker.database.entities.Area;
-import com.example.planttracker.database.entities.Plant;
 import com.example.planttracker.database.entities.User;
-import com.example.planttracker.utilities.LightLevel;
 
 import org.junit.After;
 import org.junit.Before;
@@ -28,16 +23,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.time.LocalDateTime;
-
 @RunWith(AndroidJUnit4.class)
 public class DatabaseUserTest {
     @Rule
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
     private static AppDatabase db;
     private static UserDAO userDAO;
-    private static AreaDAO areaDAO;
-    private static PlantDAO plantDAO;
 
     @Before
     public void CreateDB() {
@@ -45,30 +36,12 @@ public class DatabaseUserTest {
 
         db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
         userDAO = db.userDAO();
-        areaDAO = db.areaDAO();
-        plantDAO = db.plantDAO();
-
-        LiveData<User> userObserver;
 
         // Create default users:
         User testAdmin1 = new User("admin1", "admin1", true);
         User testUser1 = new User("testUser1", "testUser1");
 
         userDAO.insert(testAdmin1, testUser1);
-
-        // Create default areas
-        Area testArea1 = new Area(1, "Living Room", 1, LightLevel.MEDIUM);
-        Area testArea2 = new Area(1, "Bedroom", 1, LightLevel.LOW);
-        Area testArea3 = new Area(1, "Balcony", 1, LightLevel.HIGH);
-
-        areaDAO.insert(testArea1, testArea2, testArea3);
-
-        // Create default plants
-        Plant testPlant1 = new Plant(1, 1, "Mr. Leaves", "Fern", LightLevel.LOW, 3, LocalDateTime.now());
-        Plant testPlant2 = new Plant(1, 2, "Leaf Erikson", "Shrub", LightLevel.MEDIUM, 7, LocalDateTime.now());
-        Plant testPlant3 = new Plant(1, 3, "Cooler Cactus", "Succulent", LightLevel.HIGH, 14, LocalDateTime.now());
-
-        plantDAO.insert(testPlant1, testPlant2, testPlant3);
     }
 
     @After
